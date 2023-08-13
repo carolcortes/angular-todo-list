de42f5696ad8f9994b11ad06d75f47a4fe45f738
carol <caroline.ocortes@gmail.com>
2023-08-13 14:37:04 +0100

    create angular project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'todo', pathMatch: 'full' },
+  { path: 'todo', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
